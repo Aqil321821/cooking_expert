cc82abf35decfd24bcf6c5bbc0323cd33c4466e9
Aqil321821 <aqilrao321821@gmail.com>
2024-09-17 03:56:06 +0500

    Initial commit

---
+++ src/pages/recipe/Recipe.js
@@ -0,0 +1,41 @@
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useFetch } from '../../hooks/useFetch';
+
+// styles
+import './Recipe.css';
+
+export default function Recipe() {
+  const { id } = useParams();
+  const url = 'http://localhost:3000/recipes/' + id;
+  const { error, isPending, data: recipe } = useFetch(url);
+
+  useEffect(() => {
+    if (recipe) {
+      console.log(recipe.ingredients); // Log ingredients to the console for debugging
+    }
+  }, [recipe]); 
+  
+  return (
+    <div className='recipe'>
+   
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className='page-title'>{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+
+          <ul>
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+          <p className='method'>{recipe.method}</p>
+        </>
+        
+      )}
+    </div>
+  );
+  
+}
