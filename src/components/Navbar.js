3098b5b21e5032e4705c1a8297ae1a0ae5d249f1
Aqil321821 <aqilrao321821@gmail.com>
2024-09-21 02:13:22 +0500

    making a context state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-
+import { useTheme } from '../hooks/useTheme';
 import Searchbar from './Searchbar';
 
 import './Navbar.css';
 
 export default function Navbar() {
+  const { color, changeColor } = useTheme();
+
   return (
-    <div className='navbar'>
-      <nav>
+    <div className='navbar' style={{ background: color }}>
+      <nav onClick={() => changeColor('green')}>
         <Link to='/' className='brand'>
           <h1>Cooking Expert</h1>
         </Link>
